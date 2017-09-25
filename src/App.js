9d47ee2da2a7adfa863aeec23d7cdaea14737d8a
paritosh_pundir <0p0a0r0i@gmail.com>
2017-09-26 00:07:43 +0530

    setup Flow in react module

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+// @flow
+
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+type Props = {
+  value: string,
+  onChange: (value: string) => mixed,
+};
+type State = {
+  count: number,
+}
+
+class App extends Component<Props, State> {
+  props: Props;
+  state: State = {count: 10};
+
   render() {
+    let clickMe = () => {
+      this.setState({
+        count: 30,
+      });
+    }
+
+
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React with Flow and Jest</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro" onClick={() => clickMe()}>
+          To get started, edit <code>{this.state.count}</code> and save to reload.
         </p>
       </div>
     );
